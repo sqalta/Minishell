867ba13aa54b73cd8c72ea06ea05d0ec15891a78
Şerif <pltserif@gmail.com>
2023-06-06 18:44:18 +0300

    command_line malloc fixed try3

--- srcs/command_line.c
+++ srcs/command_line.c
@@ -6,7 +6,7 @@
 /*   By: spalta <spalta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 19:29:09 by mkarakul          #+#    #+#             */
-/*   Updated: 2023/06/06 16:49:54 by spalta           ###   ########.fr       */
+/*   Updated: 2023/06/06 18:41:27 by spalta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,33 @@ void	ft_command_line2(t_arg *temp, int i, int j)
 		g_data.list = g_data.list->next;
 }
 
+void	ft_command_redirection_len(t_arg *temp, int i, int j)
+{
+	while (temp && temp->type != PIPE)
+	{
+		if (temp->type == WORD || temp->type == DOLLAR)
+			i++;
+		else if (temp->type == DOUBLE_INPUT_RDR)
+			temp = temp->next;
+		else if (temp->type == OUTPUT_RDR || temp->type == INPUT_RDR
+			|| temp->type == DOUBLE_OUTPUT_RDR)
+		{
+			j++;
+			j++;
+			temp = temp->next;
+		}
+		temp = temp->next;
+	}
+	g_data.list = temp;
+	g_data.command = malloc(sizeof(char *) * i + 1); //kontrolle!
+	g_data.redirection = malloc(sizeof(char *) * j + 1);
+	if (!temp)
+		return ;
+	if (g_data.list->type == PIPE)
+		g_data.list = g_data.list->next;
+}
+
+
 void	ft_command_line(void)
 {
 	t_arg	*temp;
@@ -47,7 +74,22 @@ void	ft_command_line(void)
 	i = 0;
 	j = 0;
 	temp = g_data.list;
-	g_data.command = malloc(sizeof(char *) * (g_data.count_type->word - g_data.count_type->rdr)); //kontrolle!
-	g_data.redirection = malloc(sizeof(char *) * g_data.count_type->rdr * 2);
+	ft_command_redirection_len(temp,i ,j);
 	ft_command_line2(temp, i, j);
+	/*
+	while (g_data.command[i])
+	{
+		printf ("%s\n", g_data.command[i]);
+		i++;
+	}
+	i = 0;
+	j = 0;
+	while (g_data.redirection[i])
+	{
+		getchar();
+		printf ("%s\n", g_data.redirection[i]);
+		i++;
+	}
+	exit (1);
+	*/
 }
